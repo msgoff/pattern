5fed660de87a6e9dedcdfa1aee5c815cbdc616d6
mike <mike@lcd-outlet.com>
2019-12-20 17:45:07 +0000

    alpine

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 virtualenv venv --python=python3.7
 source venv/bin/activate
-sudo apt install libmariadbclient-dev
+
+alpine="$(grep -c vanilla <<"$(uname -r)")"
+
+if [[ $alpine -eq 1 ]]
+then
+	apk add --no-cache mariadb-connector-c-dev
+else
+	sudo apt install libmariadbclient-dev
+fi
+
+
 python setup.py install
