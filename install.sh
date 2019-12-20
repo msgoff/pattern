09d053010bf65fa4beeac8d37ca73d1f46127cbe
mike <mike@lcd-outlet.com>
2019-12-20 18:42:52 +0000

    alpine

--- install.sh
+++ install.sh
@@ -6,6 +6,19 @@ alpine="$(grep -c vanilla <<"$(uname -r)")"
 if [[ $alpine -eq 1 ]]
 then
 	apk add --no-cache mariadb-connector-c-dev
+	apk add lapack
+	apk add cython
+	apk add py3-numpy
+	apk add lapack-dev
+	apk add blas-dev
+	apk add py3-scipy
+	apk add libxml2-dev libxslt-dev
+	apk add gfortran
+	wget https://github.com/xianyi/OpenBLAS/archive/v0.3.6.tar.gz \
+	&& tar -xf v0.3.6.tar.gz \
+	&& cd OpenBLAS-0.3.6/ \
+	&& make BINARY=64 FC=$(which gfortran) USE_THREAD=1 \
+	&& make PREFIX=/usr/lib/openblas install
 else
 	sudo apt install libmariadbclient-dev
 fi
